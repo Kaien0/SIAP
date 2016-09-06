bb9c29b742e67fbe22609171f116b5529a0880ac
Kaien0 <Alexandre.Khalfaoui@gmail.com>
2016-09-06 09:00:42 +0200

    Test push GitHub depuis MacAK

--- app/src/main/java/com/example/toyo/barcodereader/MainActivity.java
+++ app/src/main/java/com/example/toyo/barcodereader/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.toyo.barcodereader;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -10,63 +11,32 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-//Test Github Mac
-public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+public class MainActivity extends Activity {
     private Button scanBtn, loginBtn, formulaireBtn;
     private TextView formatTxt, contentTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.logo); //activity_main
 
-        formulaireBtn = (Button)findViewById(R.id.BtnFormulaireParc);
-        formulaireBtn.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, formulaireparcActivity.class);
-                startActivity(intent);
+        final Thread ThBienvenue = new Thread() {
+            public void run() {
+
+                try {
+                    sleep(5000);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+
+                } finally {
+                    Intent i = new Intent(MainActivity.this, AccueilActivity.class);
+                    startActivity(i);
+                    finish();
+                }
             }
-        });
+        };
+        ThBienvenue.start();
 
-        scanBtn = (Button)findViewById(R.id.scan_button);
-        //Bouton changement de vue
-        loginBtn = (Button)findViewById(R.id.btn_Login);
-        loginBtn.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
-        formatTxt = (TextView)findViewById(R.id.scan_format);
-        contentTxt = (TextView)findViewById(R.id.scan_content);
-
-        scanBtn.setOnClickListener(this);
-    }
-
-    public void onClick(View v){
-        if(v.getId()==R.id.scan_button){
-
-            IntentIntegrator scanIntegrator = new IntentIntegrator(this);
-            scanIntegrator.initiateScan();
-        }
-    }
-
-
-
-    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
-        if (scanningResult != null) {
-            String scanContent = scanningResult.getContents();
-            String scanFormat = scanningResult.getFormatName();
-            formatTxt.setText("Format : " + scanFormat);
-            contentTxt.setText("Code barre : " + scanContent);
-        }
-        else{
-            Toast toast = Toast.makeText(getApplicationContext(),
-                    "No scan data received!", Toast.LENGTH_SHORT);
-            toast.show();
-        }
-    }
-}
+}}
