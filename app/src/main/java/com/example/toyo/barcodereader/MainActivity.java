6906237c90d22c280e864ef5045875f0cd5f025b
Kaien0 <Alexandre.Khalfaoui@gmail.com>
2016-09-06 09:09:10 +0200

    Version de 9h09

--- app/src/main/java/com/example/toyo/barcodereader/MainActivity.java
+++ app/src/main/java/com/example/toyo/barcodereader/MainActivity.java
@@ -1,20 +1,10 @@
 package com.example.toyo.barcodereader;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
 import android.content.Intent;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
-    private Button scanBtn, loginBtn, formulaireBtn;
-    private TextView formatTxt, contentTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
