44b25fc83d3dede3eb3b0c673215be6a5c9efcda
Kaien0 <Alexandre.Khalfaoui@gmail.com>
2016-10-06 03:00:18 +0200

    version propre

---
+++ app/src/main/java/com/SIAP/Classes/Question.java
@@ -0,0 +1,96 @@
+package com.SIAP.Classes;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+
+/**
+ * Created by bashark on 11/09/2016.
+ */
+public class Question {
+   private ArrayList<Reponse> LST_reponse;
+    private String id;
+    private String nom;
+    private String texte;
+    private Context ctx;
+   // private Context ctxQ;
+
+
+   /* public  Question(int id_quest, Context ctx){
+        id = id_quest;
+        this.ctxQ = ctx;
+        BackgroundTask bg = new BackgroundTask(this.ctxQ);
+        bg.execute("constructQuestion", String.valueOf(id));
+    }*/
+
+
+//    public Question(Context ctx, String id) {
+//        final BackgroundTask recupQuestions = new BackgroundTask(ctx);
+//        String geet;
+//        String questt;
+//        String[] EXTRA_TabQuest;
+//        String EXTRA_TabConstructQuestion = null;
+//
+//        try {
+//            geet = recupQuestions.execute("constructeurQuestion", id).get();
+//            EXTRA_TabQuest = geet.split("<br/>");
+//            this.id = EXTRA_TabQuest[0];
+//            this.nom = EXTRA_TabQuest[1];
+//            this.texte = EXTRA_TabQuest[2];
+//            LST_reponse = new ArrayList<Reponse>();
+//
+//            int r = 3;
+//            while(r < EXTRA_TabQuest.length)
+//            {
+//                LST_reponse.add(new Reponse(ctx, EXTRA_TabQuest[r]));
+//                r++;
+//            }
+//
+//        } catch (InterruptedException e1) {
+//            e1.printStackTrace();
+//        } catch (ExecutionException e1) {
+//            e1.printStackTrace();
+//        } catch (ArrayIndexOutOfBoundsException e2){
+//            e2.printStackTrace();
+//        }catch (IndexOutOfBoundsException e){
+//            e.printStackTrace();
+//        }
+//
+//    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getTexte() {
+        return texte;
+    }
+
+    public void setTexte(String texte) {
+        this.texte = texte;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public ArrayList<Reponse> getLST_reponse() {
+
+        return LST_reponse;
+    }
+    public Reponse getReponse(int r) {
+        if (r < LST_reponse.size()){
+            return LST_reponse.get(r);
+        }
+        return null;
+    }
+
+}
